fd8e8d1be2bb98d3c0caf620c3c0c2756fa60fe0
Batname <dadubinin@gmail.com>
2017-07-16 10:19:32 +0200

    init

---
+++ Source/TwinStickShooter_CPP/TwinStickShooter_CPP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TwinStickShooter_CPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter_CPP, "TwinStickShooter_CPP" );
---
+++ Source/TwinStickShooter_CPP/TwinStickShooter_CPPGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwinStickShooter_CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSTICKSHOOTER_CPP_API ATwinStickShooter_CPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
